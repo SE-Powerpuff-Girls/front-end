7d8dd1f270f8e36552a1afe343ba4316858bd577
Denise <aldeadenise@yahoo.com>
2022-04-18 22:55:05 +0300

    create conference page skeleton

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
 import Register from "./Register";
+import ConferencePage from "./ConferencePage"
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 						<Route path="/" element={<Home />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/register" element={<Register />} />
+						<Route path="/conferencePage" element={<ConferencePage />} />
 					</Routes>
 				</div>
 				<Footer />
---
+++ src/ConferencePage.js
@@ -0,0 +1,47 @@
+const ConferencePage = () => {
+    return (
+        <div className="conferencePage">
+            <div className="mainImage">
+                <p>Conference Header Image</p>
+            </div>
+            <div className="conferenceInformation">
+                <div className="conferenceImage">
+                    <p>Conference Image</p>
+                </div>
+                <div className="conferenceDetails">
+                    <p>Conference Title</p>
+                    <p>Conference Location</p>
+                    <div className="topics">
+                        <p>Topic 1</p>
+                        <p>Topic 2</p>
+                        <p>Topic 3</p>
+                    </div>
+                    <p>URL: www.someurl.com</p>
+                </div>
+                <div className="conferencePresentation">
+                    <h1>Presentation</h1>
+                    <p>Organic growth create spaces to explore what's next. 
+                        Lean into that problem player-coach but we need to get the 
+                        vernacular right turd polishing, but run it up the flagpole, 
+                        ping the boss and circle back nor feed the algorithm. The 
+                        closest elephant is the most dangerous this proposal is a 
+                        win-win situation which will cause a stellar paradigm shift, 
+                        and produce a multi-fold increase in deliverables close the loop 
+                        note for the previous submit: the devil should be on the left 
+                        shoulder out of the loop. Rehydrate the team i've been doing some 
+                        research this morning and we need to better nor optimize the fireball. 
+                        Increase the resolution, scale it up we need a larger print. </p>
+                </div>
+                <div className="conferenceOrganisers">
+                    <h2>Organisers</h2>
+                    <p>Organiser 1</p>
+                    <p>Organiser 2</p>
+                    <p>Organiser 3</p>
+                </div>
+                <button className="seePapersButton">See Papers</button>
+            </div>
+        </div>
+    );
+}
+
+export default ConferencePage;
