9c01bc7e69920ff579d6691636c4d1d39c3bd7d5
911 Apavaloaei Alexandru-Teodor <72084810+alexteo24@users.noreply.github.com>
2022-04-19 08:00:50 +0300

    Fixing merge issues

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
 import Register from "./Register";
-import ConferencePage from "./ConferencePage"
+import ConferencePage from "./ConferencePage";
 
 function App() {
 	return (
