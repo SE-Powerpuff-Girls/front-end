89c118a0dca6b9e800d55818c30e2d6069abeaee
911 Apavaloaei Alexandru-Teodor <72084810+alexteo24@users.noreply.github.com>
2022-05-22 01:05:17 +0300

    Add conference page and modifications for navbag, login, profilepage, register

--- src/ConferencePage.js
+++ src/ConferencePage.js
@@ -1,4 +1,5 @@
 import styles from "./ConferencePage.module.css";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const ConferencePage = () => {
@@ -14,18 +15,19 @@ const ConferencePage = () => {
 						<span className={styles["conference-location"]}>Conference Location</span>
 						<span className={styles["conference-date"]}>Date: 22.05.2024</span>
 					</div>
-					<div className={styles["topics"]}>
-						<div className={styles["topic"]}>Topic 1</div>
-						<div className={styles["topic"]}>Topic 1</div>
-						<div className={styles["topic"]}>Topic 1</div>
+					<div className={styles["topics-see-more"]}>
+						<div className={styles["topics"]}>
+							<div className={styles["topic"]}>Topic 1</div>
+							<div className={styles["topic"]}>Topic 1</div>
+							<div className={styles["topic"]}>Topic 1</div>
+						</div>
+						<button className={styles["see-papers-button"]}>See Papers</button>
 					</div>
 					<div className={styles["conference-url"]}>
 						<b>URL:</b>
 						www.someurl.com
 					</div>
 				</div>
-
-				<button className={styles["seePapersButton"]}>See Papers</button>
 			</div>
 			<div className={styles["conference-presentation"]}>
 				<h1>Presentation</h1>
@@ -34,18 +36,32 @@ const ConferencePage = () => {
 					polishing, but run it up the flagpole, ping the boss and circle back nor feed the algorithm. The closest elephant is the most dangerous this
 					proposal is a win-win situation which will cause a stellar paradigm shift, and produce a multi-fold increase in deliverables close the loop
 					note for the previous submit: the devil should be on the left shoulder out of the loop. Rehydrate the team i've been doing some research
-					this morning and we need to better nor optimize the fireball. Increase the resolution, scale it up we need a larger print.{" "}
+					this morning and we need to better nor optimize the fireball. Increase the resolution, scale it up we need a larger print.
 				</p>
 			</div>
 			<div className={styles["conference-organizers"]}>
 				<h2>Organizers</h2>
 				<div className={styles["organizers-list"]}>
-					<div className={styles["organizer"]}>Organiser 1</div>
-					<div className={styles["organizer"]}>Organiser 2</div>
-					<div className={styles["organizer"]}>Organiser 3</div>
-					<div className={styles["organizer"]}>Organiser 4</div>
+					<div className={styles["organizer"]}>
+						<img src={`${process.env.PUBLIC_URL}/Img/profilePicture.png`}></img>
+						Organiser 1
+					</div>
+					<div className={styles["organizer"]}>
+						<img src={`${process.env.PUBLIC_URL}/Img/profilePicture.png`}></img>
+						Organiser 2
+					</div>
+					<div className={styles["organizer"]}>
+						<img src={`${process.env.PUBLIC_URL}/Img/profilePicture.png`}></img>
+						Organiser 3
+					</div>
+					<div className={styles["organizer"]}>
+						<img src={`${process.env.PUBLIC_URL}/Img/profilePicture.png`}></img>
+						Organiser 4
+					</div>
 				</div>
 			</div>
+			<button className={styles["see-papers"]}>See papers</button>
+			<Footer />
 		</div>
 	);
 };
--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,10 +1,125 @@
 import Navbar from "./Navbar";
 import styles from "./ProfilePage.module.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import Footer from "./Footer";
 
 const ProfilePage = () => {
-	const { id } = useParams();
-	console.log(id);
+	let { id } = useParams();
+	id = id.replace(":", "");
+
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	const [readData, setReadData] = useState(false);
+
+	const [disabled, setDisabled] = useState(() => {
+		return !(id === JSON.parse(localStorage.getItem("user")).userid);
+	});
+
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [title, setTitle] = useState("");
+	const [gender, setGender] = useState("");
+	const [nationality, setNationality] = useState("");
+	const [address, setAddress] = useState("");
+
+	const [topics, setTopics] = useState([]);
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!readData) {
+			setReadData(true);
+			setLoading(false);
+			fetch(`${process.env.REACT_APP_API_LINK}users/${id}`, {
+				method: "GET",
+				headers: { "Content-Type": "application/json" },
+			})
+				.then((response) => {
+					if (response.status === 200) {
+						return response.json();
+					}
+					throw response;
+				})
+				.then((data) => {
+					const information = data;
+					setFirstName(information.firstname);
+					setLastName(information.lastname);
+					setEmail(information.email);
+					if (information.title) {
+						setTitle(information.title);
+					}
+					if (information.gender) {
+						setGender(information.gender);
+					}
+					if (information.nationality) {
+						setNationality(information.nationality);
+					}
+					if (information.address) {
+						setAddress(information.address);
+					}
+				})
+				.catch((error) => {
+					console.error("Error fetching data: ", error);
+					setError(error);
+				})
+				.finally(() => {
+					setLoading(false);
+				});
+			fetch(`${process.env.REACT_APP_API_LINK}users/${id}/topics`, {
+				method: "GET",
+				headers: { "Content-Type": "application/json" },
+			}).then((response) => {
+				if (response.status === 200) {
+					response.json().then((data) => {
+						setTopics(data);
+					});
+				}
+			});
+		}
+	}, []);
+
+	if (loading) {
+		return "Loading...";
+	}
+	if (error) {
+		return "Error...";
+	}
+	const handleSubmitUpdate = (e) => {
+		e.preventDefault();
+		const updateUser = { email, firstName, lastName, gender, title, nationality, address };
+		const token = localStorage.getItem("token");
+		const userid = JSON.parse(localStorage.getItem("user")).userid;
+		fetch(`${process.env.REACT_APP_API_LINK}users/${userid}`, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json", authorization: `Bearer ${token}` },
+			body: JSON.stringify(updateUser),
+		}).then((response) => {
+			if (response.status === 200) {
+				console.log("Update sucessful");
+			}
+		});
+	};
+
+	function handleSubmitDelete(e) {
+		e.preventDefault();
+		const token = localStorage.getItem("token");
+		const userid = JSON.parse(localStorage.getItem("user")).userid;
+		fetch(`${process.env.REACT_APP_API_LINK}users/${userid}`, {
+			method: "DELETE",
+			headers: { "Content-Type": "application/json", authorization: `Bearer ${token}` },
+		}).then((response) => {
+			if (response.status === 200) {
+				console.log("User deleted!");
+				localStorage.clear();
+				navigate("/login");
+			}
+		});
+	}
+
 	return (
 		<div className={styles["profile-page"]}>
 			<Navbar />
@@ -12,45 +127,59 @@ const ProfilePage = () => {
 				<div className={styles["profile-container"]}>
 					<div className={styles["profile-header"]}>
 						<div className={styles["profile-picture"]}></div>
-						<p>
-							<span>First nameeeeeeeeeeeeeeeeee</span>
-							<span>Last nameeeeeeeeeeeeeeeeeee</span>
-						</p>
+						<div className={styles["name-and-interests"]}>
+							<span>
+								{firstName} {lastName}
+							</span>
+							<br></br>
+							<br></br>
+							Interested in:
+							<div className={styles["tags"]}>
+								{topics.map((topic) => (
+									<div className={styles["tag"]}>{topic.name}</div>
+								))}
+							</div>
+						</div>
 					</div>
 					<div className={styles["profile-information"]}>
-						<form className={styles["form-update-profile-information"]}>
+						<form onSubmit={handleSubmitUpdate} className={styles["form-update-profile-information"]}>
 							<div className={styles["label-input"]}>
 								<label>Email: </label>
-								<input type="email" name="email" required></input>
+								<input disabled={disabled} onChange={(e) => setEmail(e.target.value)} type="email" name="email" required value={email}></input>
 							</div>
 							<div className={styles["label-input"]}>
 								<label>First name: </label>
-								<input type="text" required></input>
+								<input disabled={disabled} onChange={(e) => setFirstName(e.target.value)} type="text" required value={firstName}></input>
 							</div>
 							<div className={styles["label-input"]}>
 								<label>Last name: </label>
-								<input type="text" required></input>
+								<input disabled={disabled} onChange={(e) => setLastName(e.target.value)} type="text" required value={lastName}></input>
 							</div>
 							<div className={styles["label-input"]}>
 								<label>Title: </label>
-								<input type="text"></input>
+								<input disabled={disabled} onChange={(e) => setTitle(e.target.value)} type="text" value={title}></input>
 							</div>
 							<div className={styles["label-input"]}>
 								<label>Gender: </label>
-								<input type="text"></input>
+								<input disabled={disabled} onChange={(e) => setGender(e.target.value)} type="text" value={gender}></input>
 							</div>
 							<div className={styles["label-input"]}>
 								<label>Nationality: </label>
-								<input type="text"></input>
+								<input disabled={disabled} onChange={(e) => setNationality(e.target.value)} type="text" value={nationality}></input>
 							</div>
 							<div className={styles["label-input"]}>
 								<label>Address: </label>
-								<input type="text"></input>
+								<input disabled={disabled} onChange={(e) => setAddress(e.target.value)} type="text" value={address}></input>
+							</div>
+							<div className={styles["buttons"]}>
+								{!disabled && <button type="submit">Save</button>}
+								{!disabled && <button onClick={handleSubmitDelete}>Delete</button>}
 							</div>
 						</form>
 					</div>
 				</div>
 			</div>
+			<Footer />
 		</div>
 	);
 };
